4a8f1434ce7e9539dfcf3b920f340f283507279f
Rudra007 <rudrakshkachhawa@gmail.com>
2025-04-22 12:44:19 +0530

    test

--- src/screens/loginScreen.tsx
+++ src/screens/loginScreen.tsx
@@ -16,7 +16,11 @@ const LoginScreen = () => {
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             {/* <Button title="Login" onPress={handleLogin} /> */}
             {isLoading && <Text>Loading user info...</Text>}
-            <ShowGoogleLoginButton referenceId="870623a1697443499652ceeab330e5" onLoginSuccess={(data)=>{
+            <ShowGoogleLoginButton 
+            // referenceId="870623a1697443499652ceeab330e5"  // SOCKET PROD
+            referenceId="1258584g170245213365795ba5a63ab"  // PROD-HITMAN
+            config={null}
+            onLoginSuccess={(data)=>{
                 console.log(data)
             }} onLoginFailure={(data)=>{
                 console.log("Login failed",data)
